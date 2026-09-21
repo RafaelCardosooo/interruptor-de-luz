74587bc22a0cc8d62898d66917d7c8afad0d51e3
Rafael Cardoso <rafaeljesuscardoso14@gmail.com>
2026-09-21 02:35:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const isDay = false;
+const isLightOn = !isDay;
+console.log("DayTime");
+console.log(isDay);
+console.log("lights on?");
+console.log(isLightOn);
+const batteryLevel = 5;
+console.log("Nível da bateria?");
+console.log(batteryLevel + "%");
+const isWorking = isLightOn === true && batteryLevel > 0;
+console.log("tudo funcionando?");
+console.log(isWorking);
+// parte de visualização
+document.getElementById("status-dia").textContent = isDay ? "Período: Dia" : "Período: Noite";
+document.getElementById("status-luz").textContent = isLightOn ? "Luz: Ligada" : "Luz: Desligada";
+document.getElementById("status-final").textContent = isWorking ? "Sistema operacional" : "Sistema inoperante";
